74ec0bc991dded9e4a9056112466dbd59b14ff61
Rosh <roshan.dan.koshy@gmail.com>
2022-08-05 10:10:44 +0530

    init

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
+
+
+int (vector<int> prison) {
+        if(prison.at(0) == 0) return 0;
+        else{
+            int freedNumber = 0;
+            int i = 0;
+            while (i < prison.size()){
+                if(prison.at(i) == 1){
+                    freedNumber++;
+                    int j = i;
+                    while (j < prison.size()){
+                        if(prison.at(j) == 0) prison[j] = 1;
+                        else prison[j] = 0;
+                        j++;
+                    }
+                }
+                i++;
+            }
+            return freedNumber;
+        }
+    }
+
 int main ()
 {
-cout<<"Git first time ";
+    int a ;
+cout<<"Write the order of the cells "<<endl;
+vector<int>fir;
+for (int i = 0; i < 7; i++)
+{
+    cin>>a;
+    fir.push_back(a);
+    cout<<"  ";
+}
+cout<<"number of freed cells are  "<<cells(fir);
+
+
+
 }
---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+int main ()
+{
+cout<<"Enter the order of prisoners ";
+
+}
